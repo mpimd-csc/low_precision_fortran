dfe4e0c23adcd8a44fed6cd59017ee5d3f3a7a0b
Martin Köhler <koehlerm@mpi-magdeburg.mpg.de>
2025-06-26 12:34:40 +0200

    Check in

---
+++ examples/benchmark/benchmark.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern "C" void test_linspace_();
+
+int main(int argc, char *argv[])
+{
+    test_linspace_();
+    return 0;
+}
---
+++ examples/benchmark/linspace.f90
@@ -0,0 +1,23 @@
+SUBROUTINE benchmark_linspace()
+    USE FP16_SUPPORT
+    IMPLICIT NONE
+
+    INTEGER, PARAMETER :: N = 1024
+    TYPE(FP16) :: x(N+1)
+    TYPE(FP16) :: y(N+1)
+    TYPE(FP16) :: a, b, h
+    INTEGER :: K
+
+    a = 0
+    b = 1
+    h = (b - a) / FP16(N)
+
+    DO K = 1, N+1
+        x(k) = a + FP16(K-1) * h
+        y(k) = 0.625 * x(k)**2 + 0.125*x  + 1.0
+    END DO
+
+    WRITE(*,*) x, y
+END SUBROUTINE
+
+
